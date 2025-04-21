c16a0812380d9b69629d78ab2f311877d9e7b5d2
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-21 15:07:38 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -10,7 +10,7 @@ echo This might take a few moments depending on your internet speed
 haxelib install hxcpp
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
-haxelib install flixel 4.11.0
+haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
 haxelib install flixel-ui 2.5.0
