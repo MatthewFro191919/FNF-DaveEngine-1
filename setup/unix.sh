dddeff6333f6576158e86c53241d8384a48a5b89
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-21 15:12:27 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -6,7 +6,7 @@ cd ..
 echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
-echo This might take a few moments depending on your internet speed
+echo This might take a few moments depending on your internet speed.
 haxelib install hxcpp
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
